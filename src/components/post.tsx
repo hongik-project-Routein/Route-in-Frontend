4d18920a14287acc36385d58353e4481b9c669dd
김진호 <rlawlsgh1227@gmail.com>
2023-05-04 15:03:05 +0900

    design: Create post frame 게시물 프레임을 만들었습니다.

---
+++ src/components/post.tsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHeart,
+  faComment,
+  faBookmark,
+} from '@fortawesome/free-solid-svg-icons'
+
+const postText: string = `#김포공항
+    김포공항은 좋았다.
+    #제주 레포츠랜드
+    레포츠랜드는 재밌었다.
+    #스누피가든 제주
+    스누피가 너무 귀여웠다.
+    #제주여행 #제주 익사이팅
+    이번 제주도 여행은~~~~
+`
+
+export default function Post(): JSX.Element {
+  return (
+    <>
+      <PersonalInfoContainer>
+        <UserContent>
+          <Profile src="https://avatars.githubusercontent.com/u/81083461?v=4" />
+          <Nickname>jinhokim98</Nickname>
+          <DistanceFromMe>나와의 거리: 100km</DistanceFromMe>
+        </UserContent>
+        <RestContent>
+          <NumOfHeart>좋아요: 100</NumOfHeart>
+          <Icons>
+            <Heart>
+              <FontAwesomeIcon icon={faHeart} />
+            </Heart>
+            <DirectMessage>
+              <FontAwesomeIcon icon={faComment} />
+            </DirectMessage>
+            <Bookmark>
+              <FontAwesomeIcon icon={faBookmark} />
+            </Bookmark>
+          </Icons>
+        </RestContent>
+      </PersonalInfoContainer>
+      <PostImageContainer>
+        <PostImage src="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycGxhbmV8ZW58MHx8MHx8&w=1000&q=80" />
+      </PostImageContainer>
+      <PostText>{postText}</PostText>
+    </>
+  )
+}
+
+const PersonalInfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 582px;
+  height: 60px;
+`
+
+const UserContent = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+const Profile = styled.img`
+  width: 56px;
+  height: 56px;
+  margin-right: 20px;
+  border-radius: 50%;
+  object-fit: cover;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const Nickname = styled.span`
+  margin-right: 20px;
+  font-size: 16px;
+`
+
+const RestContent = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const DistanceFromMe = styled.span`
+  font-size: 16px;
+`
+
+const NumOfHeart = styled.span`
+  font-size: 16px;
+  margin-right: 20px;
+`
+
+const Icons = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`
+
+const Heart = styled.div`
+  margin-right: 10px;
+`
+
+const DirectMessage = styled.div`
+  margin-right: 10px;
+`
+
+const Bookmark = styled.div`
+  margin-right: 10px;
+`
+const PostImageContainer = styled.div`
+  width: 100%;
+  height: 582px;
+  margin-top: 20px;
+`
+const PostImage = styled.img`
+  object-fit: cover;
+  border-radius: 10px;
+`
+const PostText = styled.p`
+  margin-top: 20px;
+  padding: 8px 12px;
+  border: 1px solid #98a2b3;
+  border-radius: 8px;
+  font-size: 16px;
+  line-height: 30px;
+  white-space: pre-line;
+`
