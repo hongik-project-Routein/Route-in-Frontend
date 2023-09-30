df48fdcd3a8849af23318d8f92e4765af1ed827c
김진호 <rlawlsgh1227@gmail.com>
2023-09-30 14:49:07 +0900

    fix: 수정

--- src/mocks/data/pin.ts
+++ src/mocks/data/pin.ts
@@ -2,74 +2,74 @@ import { type SearchPinType } from '../../types/postTypes'
 
 export const pinDemo: SearchPinType[] = [
   {
-    image:
+    writer_image:
       'https://cdn.pixabay.com/photo/2021/06/25/13/22/girl-6363743_1280.jpg',
     writer: 'jinokim98',
-    pin_image: '/img/post/post1-1.jpg',
+    image: '/img/post/post1-1.jpg',
 
-    postId: 1,
+    post_id: 1,
     like_users: ['soomineom'],
-    like_status: false,
-    bookmark: false,
+    is_liked: false,
+    is_bookmarked: false,
   },
   {
-    image:
-      'https://cdn.pixabay.com/photo/2021/06/25/13/22/girl-6363743_1280.jpg',
-    writer: 'jinokim98',
-
-    pin_image: '/img/post/post1-2.jpg',
-
-    postId: 1,
-    like_users: ['soomineom'],
-    like_status: false,
-    bookmark: false,
-  },
-  {
-    image:
+    writer_image:
       'https://cdn.pixabay.com/photo/2021/06/25/13/22/girl-6363743_1280.jpg',
     writer: 'jinokim98',
 
-    pin_image: '/img/post/post1-3.jpg',
+    image: '/img/post/post1-2.jpg',
 
-    postId: 1,
+    post_id: 1,
     like_users: ['soomineom'],
-    like_status: false,
-    bookmark: false,
+    is_liked: false,
+    is_bookmarked: false,
   },
   {
-    image:
+    writer_image:
+      'https://cdn.pixabay.com/photo/2021/06/25/13/22/girl-6363743_1280.jpg',
+    writer: 'jinokim98',
+
+    image: '/img/post/post1-3.jpg',
+
+    post_id: 1,
+    like_users: ['soomineom'],
+    is_liked: false,
+    is_bookmarked: false,
+  },
+  {
+    writer_image:
       'https://cdn.pixabay.com/photo/2016/03/24/09/10/man-1276384_1280.jpg',
     writer: 'chris',
 
-    pin_image: '/img/post/post3-1.jpg',
+    image: '/img/post/post3-1.jpg',
 
-    postId: 3,
+    post_id: 3,
     like_users: ['soomineom', 'jinokim98'],
-    like_status: true,
-    bookmark: true,
+    is_liked: true,
+    is_bookmarked: true,
   },
   {
-    image:
+    writer_image:
       'https://cdn.pixabay.com/photo/2016/03/24/09/10/man-1276384_1280.jpg',
     writer: 'chris',
 
-    pin_image: '/img/post/post3-2.jpg',
+    image: '/img/post/post3-2.jpg',
 
-    postId: 3,
+    post_id: 3,
     like_users: ['soomineom', 'jinokim98'],
-    like_status: true,
-    bookmark: true,
+    is_liked: true,
+    is_bookmarked: true,
   },
   {
-    image:
+    writer_image:
       'https://cdn.pixabay.com/photo/2016/12/19/21/36/woman-1919143_1280.jpg',
     writer: 'Sophia',
 
-    pin_image: '/img/post/post5-2.jpg',
+    image: '/img/post/post5-2.jpg',
 
-    postId: 5,
+    post_id: 5,
     like_users: ['soomineom', 'jinokim98'],
-    like_status: true,
-    bookmark: false,
+    is_liked: true,
+    is_bookmarked: false,
   },
 ]
